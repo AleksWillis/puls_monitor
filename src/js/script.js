d9fb12facd337ebdff70a2b85191ce9e243c874a
AleksWork <sambrera2009@gmail.com>
2022-11-01 16:11:22 +0300

    release 1.2

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
+        speed: 300,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>', 
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         dots: '<button type="button" class="slick-dots"></button>',
@@ -47,17 +47,17 @@ $(document).ready(function(){
     //Modal
 
     $('[data-modal=consultation]').on('click', function() {
-        $('.overlay, #consultation').fadeIn('slow');
+        $('.overlay, #consultation').fadeIn();
     });
 
     $('.modal__close').on('click', function() {
-        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+        $('.overlay, #consultation, #thanks, #order').fadeOut();
     });
 
     $('.button_mini').each(function(i) {
         $(this).on('click', function() {
             $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-            $('.overlay, #order').fadeIn('slow');
+            $('.overlay, #order').fadeIn();
         });
     });
 
@@ -111,7 +111,7 @@ $(document).ready(function(){
         }).done(function(){
             $(this).find("input").val("");
             $('#consultation, #order').fadeOut();
-            $('.overlay, #thanks').fadeIn('slow');
+            $('.overlay, #thanks').fadeIn();
 
             $('form').trigger('reset');  
         });
