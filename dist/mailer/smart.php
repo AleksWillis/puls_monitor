226b331621ce9ba81d461810b5f1e18dc4551a3a
AleksWork <sambrera2009@gmail.com>
2023-01-25 10:26:38 +0300

    password

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -14,7 +14,7 @@ $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.yandex.by';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'alekseikarpiuk@yandex.by';                 // Наш логин
-$mail->Password = 'tinluidolpksoybo';                           // Наш пароль от ящика
+$mail->Password = 'uhiadyvytgzhmmda';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
